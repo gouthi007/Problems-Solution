33163870ebc2d8091b43c85248a96f0ad99cc353
Goutham Balasubramaniam <34099616+gouthi007@users.noreply.github.com>
2019-02-09 19:10:15 -0500

    Add files via upload

---
+++ Leetcode_rotateddigits.py
@@ -0,0 +1,36 @@
+'''X is a good number if after rotating each digit individually by 180 degrees, we get a valid number that is different from X.  Each digit must be rotated - we cannot choose to leave it alone.
+
+A number is valid if each digit remains a digit after rotation. 0, 1, and 8 rotate to themselves; 2 and 5 rotate to each other; 6 and 9 rotate to each other, and the rest of the numbers do not rotate to any other number and become invalid.
+
+Now given a positive number N, how many numbers X from 1 to N are good?
+
+Example:
+Input: 10
+Output: 4
+Explanation: 
+There are four good numbers in the range [1, 10] : 2, 5, 6, 9.
+Note that 1 and 10 are not good numbers, since they remain unchanged after rotating.
+Note:
+
+N  will be in range [1, 10000].
+'''
+
+class Solution:
+    def rotatedDigits(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        answer = 0
+        for i in range(N + 1):
+            i = str(i)
+            if '3' in i or '4' in i or '7' in i:
+                pass
+            else:
+                if '2' in i or '5' in i or '6' in i or '9' in i :
+                    answer += 1
+        return(answer)
+
+mysolution = Solution()
+print(mysolution.rotatedDigits(10))
+             
