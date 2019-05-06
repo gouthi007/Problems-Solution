136e9c876dd064dc0c2f3c7b8ba03163d971ac42
Goutham Balasubramaniam <34099616+gouthi007@users.noreply.github.com>
2019-05-06 13:48:56 -0400

    Update Linkedlsit.c

--- Linkedlsit.c
+++ Linkedlsit.c
@@ -8,100 +8,126 @@ struct node{
 
 struct node *head = NULL;
 struct node *current = NULL;
-int count = 0;
-void printfirst()
-{
-    if(head == NULL)
-    {
-        printf("asfsaa0\n");
-        return;
-    
-    }else
-    {
-        struct node *current = head;
-        for(current = head; current != NULL; current = current->next)
-                printf("%d\t", current->data);
-    }
-}
 
-void insertfirst(int num)
+int count = 0;
+
+void insertfirst(int datain)
 {
     struct node *new;
     new = malloc(sizeof(struct node));
-    new->data = num;
+    new->data = datain;
     new->next = head;
     head = new;
     count += 1;
+    printf("insert succ\n");
 }
 
-void accuratedelete(int data)
+void printfirst()
+{
+    struct node *current;
+    current = head;
+    if (head == NULL)
+    {
+        printf("empty\n");
+        return;
+    }
+    
+    else
+    {
+        for(current = head; current != NULL; current = current->next )
+            printf("Data: %d\n", current->data);
+        
+    }
+}
+
+void deletefirst()
+{
+    if(head == NULL)
+    {    
+        printf("empty list");
+        return;
+    } 
+    
+    printf("Deleted Number : %d\n", head->data);
+    head = head->next;
+    count -= 1;
+    
+}
+
+void isempty()
+{
+    return count;
+}
+
+void deleteaccurate(int data)
 {
     struct node *current = head;
     struct node *previous = NULL;
     
-    if(current == NULL)
-        return 0;
-    while (current->data != data)
+    if(head == NULL)
+        return;
+    
+    while(current->data != data)
     {
         if(current->next == NULL)
-            return 0;
+        {
+            printf("not present");
+            return;
+        }
         else
         {
             previous = current;
             current = current->next;
         }
+    }
+    
     if(current == head)
     {
-        printf("Deketerd     %d", head->data);
+        printf("Accurate Deleted number : %d\n\n", current->data);
         head = head->next;
-        count -= 1;
     }
     else
-        {
-        printf("Deketerd     %d", current->data);
+    {
+        printf("Accurate Deleted number : %d\n\n", current->data);
         previous->next = current->next;
-        count -= 1;
-            
-        }
-            
-        }
-    return;
-}
-void deletefirst()
-{
-    printf("Deleted %d\n\n",head->data );
-    head = head->next;
-    count = count - 1;
+    }
+    
 }
 
-int isempty()
+void reverse()
 {
-    return (head == NULL);
-}
-
-void countfn()
-{
-    printf("Length = %d\n\n", count);
+    struct node *current = head;
+    struct node *previous = NULL;
+    struct node *next = NULL;
+    
+    while(current != NULL)
+    {
+        next = current->next;
+        current->next = previous;
+        previous = current;
+        current = next;
+    }
+    head = previous;
 }
 void main()
-{
-    
+
+{   
     printfirst();
-    countfn();
-    printf("\nis isempty = %d\n", isempty() );
+    insertfirst(2);
+    printfirst();
     insertfirst(3);
-    countfn();
+    printfirst();
     insertfirst(4);
-    countfn();
+    printfirst();
+    printfirst();
     insertfirst(5);
-    countfn();
-    insertfirst(6);
-    countfn();
     printfirst();
-    deletefirst();
-    countfn();
+    reverse();
+    printf("reversed\n");
     printfirst();
-    accuratedelete(4);
+    insertfirst(9);
     printfirst();
-    
+    reverse();
+    printf("reversed\n");
+    printfirst();
 }
